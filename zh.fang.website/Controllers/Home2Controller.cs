d42cf48d87689b173deb0798f7dc632c8862de37
zh.fang <ian.zh.fang@gmail.com>
2017-10-15 01:15:55 +0800

    重新设计主页

---
+++ zh.fang.website/Controllers/Home2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zh.fang.website.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
